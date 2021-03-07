7bbdc17fd971c1285fec3033005a3226545bcd57
William Heryanto <will.heryanto@gmail.com>
2021-03-07 17:58:29 +0700

    add 706 python solution

---
+++ python/0706_design_hashmap.py
@@ -0,0 +1,75 @@
+# Source : https://leetcode.com/problems/design-hashmap/
+# Author : William Heryanto
+# Date   : 2021-03-07
+
+###############################################################################
+#
+# Design a HashMap without using any built-in hash table libraries.
+#
+# To be specific, your design should include these functions:
+#
+# `put(key, value)` : Insert a (key, value) pair into the HashMap. If the value
+# already exists in the HashMap, update the value.
+# `get(key)`: Returns the value to which the specified key is mapped, or -1 if
+# this map contains no mapping for the key.
+# `remove(key)` : Remove the mapping for the value key if this map contains the
+# mapping for the key.
+#
+# Example:
+#
+# MyHashMap hashMap = new MyHashMap();
+# hashMap.put(1, 1);
+# hashMap.put(2, 2);
+# hashMap.get(1);            // returns 1
+# hashMap.get(3);            // returns -1 (not found)
+# hashMap.put(2, 1);          // update the existing value
+# hashMap.get(2);            // returns 1
+# hashMap.remove(2);          // remove the mapping for 2
+# hashMap.get(2);            // returns -1 (not found)
+#
+# Note:
+#
+# All keys and values will be in the range of `[0, 1000000]`.
+# The number of operations will be in the range of `[1, 10000]`.
+# Please do not use the built-in HashMap library.
+#
+#
+###############################################################################
+
+
+class MyHashMap:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.data = [-1]
+
+    def put(self, key: int, value: int) -> None:
+        """
+        value will always be non-negative.
+        """
+        try:
+            self.data[key]
+        except:
+            n = key - (len(self.data) - 1)
+            self.data += [-1] * n
+
+        self.data[key] = value
+
+    def get(self, key: int) -> int:
+        """
+        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+        """
+        try:
+            return self.data[key]
+        except:
+            return -1
+
+    def remove(self, key: int) -> None:
+        """
+        Removes the mapping of the specified value key if this map contains a mapping for the key
+        """
+        try:
+            self.data[key] = -1
+        except:
+            pass
