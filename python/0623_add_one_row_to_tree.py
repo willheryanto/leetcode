0b6de79140665e3f9512ee0acce47346acfbb468
William Heryanto <will.heryanto@gmail.com>
2021-03-09 22:55:26 +0700

    add 623 python solution

---
+++ python/0623_add_one_row_to_tree.py
@@ -0,0 +1,98 @@
+# Source : https://leetcode.com/problems/add-one-row-to-tree/
+# Author : William Heryanto
+# Date   : 2021-03-09
+
+###############################################################################
+#
+# Given the root of a binary tree, then value `v` and depth `d`, you need to
+# add a row of nodes with value `v` at the given depth `d`. The root node is at
+# depth 1.
+#
+# The adding rule is: given a positive integer depth `d`, for each NOT null
+# tree nodes `N` in depth `d-1`, create two tree nodes with value `v` as `N's`
+# left subtree root and right subtree root. And `N's` original left subtree
+# should be the left subtree of the new left subtree root, its original right
+# subtree should be the right subtree of the new right subtree root. If depth
+# `d` is 1 that means there is no depth d-1 at all, then create a tree node
+# with value v as the new root of the whole original tree, and the original
+# tree is the new root's left subtree.
+#
+# Example 1:
+#
+# Input:
+# A binary tree as following:
+#        4
+#      /   \
+#     2     6
+#    / \   /
+#   3   1 5
+#
+# v = 1
+#
+# d = 2
+#
+# Output:
+#        4
+#       / \
+#      1   1
+#     /     \
+#    2       6
+#   / \     /
+#  3   1   5
+#
+# Example 2:
+#
+# Input:
+# A binary tree as following:
+#       4
+#      /
+#     2
+#    / \
+#   3   1
+#
+# v = 1
+#
+# d = 3
+#
+# Output:
+#       4
+#      /
+#     2
+#    / \
+#   1   1
+#  /     \
+# 3       1
+#
+# Note:
+#
+# The given d is in range [1, maximum depth of the given tree + 1].
+# The given binary tree has at least one tree node.
+#
+#
+###############################################################################
+
+
+class Solution:
+    def addOneRow(self, root: TreeNode, v: int, d: int) -> TreeNode:
+        def _traversal(prenode, treenode, is_left=True, depth=1):
+            if treenode or depth == d:
+                cur = treenode
+                if depth == d:
+                    newnode = TreeNode(v)
+                    if is_left:
+                        newnode.left = cur
+                        prenode.left = newnode
+                    else:
+                        newnode.right = cur
+                        prenode.right = newnode
+                    cur = newnode
+                _traversal(treenode, cur.left, True, depth + 1)
+                _traversal(treenode, cur.right, False, depth + 1)
+
+        if d > 1:
+            _traversal(root, root)
+        else:
+            newnode = TreeNode(v)
+            newnode.left = root
+            root = newnode
+        return root
