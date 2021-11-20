41a8639e25348dd6ef81fcf3d18e8933c69b66e5
willheryanto <will.heryanto@gmail.com>
2021-11-20 23:26:18 +0700

    improve to handle multiple var

--- scripts/create_test_case.sh
+++ scripts/create_test_case.sh
@@ -27,7 +27,6 @@ INPUT_CONTENT="$(echo "$EXTRACTED_CONTENT" | rg Input | sed 's/.*Input: \(.*\)/\
 # Extract test numbers
 TEST_NUMBERS=$(echo "$INPUT_CONTENT" | wc -l)
 
-
 # Write to file
 echo "$TEST_NUMBERS" >$FILEPATH
 
@@ -35,31 +34,31 @@ echo "$TEST_NUMBERS" >$FILEPATH
 IFS=$'\n'
 for line in $INPUT_CONTENT; do
 	length=0
-    vars=$(echo "$line" | sed 's/, /\n/')
+	vars=$(echo "$line" | sed 's/, /\n/' | sed 's/"//g')
 
-    v=''
-    for var in $vars; do
-        tmp=$(echo $var | awk -F '= ' '{print $2}')
+	v=''
+	for var in $vars; do
+		tmp=$(echo $var | awk -F '= ' '{print $2}')
 
-        # Clean if it's array
-        if [[ $tmp == "["* && $tmp == *"]" ]]; then
-            tmp=$(echo "$tmp" | sed 's/\[//g' | sed 's/\]//g' | sed 's/ //g' | sed 's/,/ /g')
-            tmpv=$(echo $tmp | sed 's/[0-9]//g' | sed 's/-//g')
-            length=$(expr ${#tmpv} + 1)
-        fi
+		# Clean if it's array
+		if [[ $tmp == "["* && $tmp == *"]" ]]; then
+			tmp=$(echo "$tmp" | sed 's/\[//g' | sed 's/\]//g' | sed 's/ //g' | sed 's/,/ /g')
+			tmpv=$(echo $tmp | sed 's/[0-9]//g' | sed 's/[a-z]//g' | sed 's/-//g')
+			length=$(expr ${#tmpv} + 1)
+		fi
 
-        # If value exist, add space before adding value
-        if [ ${#v} -gt 0 ]; then
-            v+=" $tmp"
-        else
-            v+=$tmp
-        fi
-    done
+		# If value exist, add space before adding value
+		if [ ${#v} -gt 0 ]; then
+			v+=" $tmp"
+		else
+			v+=$tmp
+		fi
+	done
 
-    # If length greater than 0, add value to file to show array length
-    if [ $length -gt 0 ]; then
-        echo $length >>tmp
-    fi
+	# If length greater than 0, add value to file to show array length
+	if [ $length -gt 0 ]; then
+		echo $length >>tmp
+	fi
 
-    echo $v >>tmp
+	echo $v >>tmp
 done
